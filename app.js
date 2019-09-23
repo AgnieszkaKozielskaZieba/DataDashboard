f16f5c305b9dc780aef96d3ae4920143a7d1db2a
Agnieszka <agnieszka.kozielska@op.pl>
2019-09-23 10:59:23 +0200

    modified:   app.js 	modified:   index.html 	modified:   styles.css

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var countryId;
 var selYear;
 var minYear;
 var maxYear;
-var width,height, pie_width,pie_height,country_width,country_height
+var xScale, yScale;
 
 d3.queue()
 .defer(d3.json,'//unpkg.com/world-atlas@1.1.4/world/50m.json')
@@ -24,25 +24,31 @@ d3.queue()
 
 	geoData=topojson.feature(mapData,mapData.objects.countries).features;
 	emmissionsData=emData;
-	valueToCheck="emissionsPerCapita"
+	valueToCheck="emissions"
 	countryId=geoData[0].id;
 
 	minYear=d3.min(emmissionsData,d=>d.year)
 	maxYear=d3.max(emmissionsData,d=>d.year)
-	selYear=minYear;
+	selYear=maxYear;
+
+	var selYearLabel=d3.select("#selYearLabel")
+	.text("Year: "+selYear)
 
 	d3.select("#selYear")
 	.property("min",minYear)
 	.property("max",maxYear)
-	.property("value",minYear)
+	.property("value",selYear)
 	.on("input",()=>{
+		d3.event.preventDefault()
 		selYear=d3.event.target.value
+		selYearLabel.text("Year: "+selYear)
 		setColors()
 		drawPieChart()
 	})
 
 	d3.selectAll("input[name='Emissions']")
 	.on("change",()=>{
+		d3.event.preventDefault()
 		valueToCheck=d3.event.target.value
 		setColors()
 		drawPieChart()
@@ -52,28 +58,117 @@ d3.queue()
 	setColors()
 	drawPieChart()
 	drawCountryChart()
+	d3.select(window).on('resize', resizeElements);
 })
 
-function setColors(){
+function resizeElements(){
+	resizeMap();
+	resizePieChart();
+	resizeCountryChart();
+}
 
-	width=d3.select("#mapContainer").node().getBoundingClientRect().width;
-	height=2*width/3;
+function resizeCountryChart(){
+	let countryData=emmissionsData.filter(d=>d.countryCode===countryId)
+	let country_width=d3.select("#countryChartContainer").node().getBoundingClientRect().width;
+	let country_height=country_width
+	let barPadding=1;
+	let padding=50;
 
-	var projection=d3.geoMercator()
+	xScale.range([padding,country_width-padding])
+
+	yScale.range([country_height-padding,padding])
+
+	let xAxis=d3.axisBottom(xScale)
+	.tickFormat(d3.format(".0f"))
+	var yAxis=d3.axisLeft(yScale);
+
+	let bars=d3.select("#countryChart")
+	.attr("width",country_width)
+	.attr("height",country_height)
+	.selectAll(".bar")
+
+	bars
+	.select("rect")
+	.attr("x",(d,i)=>{
+		return xScale(d.x0)})
+	.attr("y",d=>yScale(d[0][valueToCheck]))
+	.attr("width",d=>{
+		if (d.x1===d.x0){
+			return xScale(d.x1+1)-xScale(d.x0)-barPadding
+		}
+		return xScale(d.x1)-xScale(d.x0)-barPadding})
+	.attr("height",d=>{
+		return country_height-padding-yScale(d[0][valueToCheck])
+	})
+
+	d3.select("#countryChart")
+	.selectAll(".axis")
+	.remove()
+
+	d3.select("#countryChart")
+	.append("g")
+	.classed("axis",true)
+	.attr("transform","translate(0,"+(country_height-padding)+")")
+	.call(xAxis)
+
+	d3.select("#countryChart")
+	.append("g")
+	.classed("axis",true)
+	.attr("transform","translate("+padding+","+(0)+")")
+	.call(yAxis)
+
+	d3.select("#countryChart")
+	.select(".countryLabel")
+		.attr("x",padding+"px")
+}
+
+function resizeMap(){
+	let width=d3.select("#mapContainer").node().getBoundingClientRect().width;
+	let height=2*width/3;
+
+	let projection=d3.geoMercator()
 	.scale(width/7)
 	.translate([width/2,height/1.4])
 
-	var path=d3.geoPath()
+	let path=d3.geoPath()
 	.projection(projection);
 
-	var colors = ["#f1c40f", "#e67e22", "#e74c3c", "#c0392b"];
+	d3.select("#map")
+	.attr("width",width)
+	.attr("height",height)
+	.selectAll(".country")
+	.attr("d",path)
+}
 
-	var domains = {
+function resizePieChart(){
+	let padding=25;
+	let pie_width=d3.select("#pieChartContainer").node().getBoundingClientRect().width;
+	let pie_height=pie_width;
+
+	let chart=d3.select("#pieChart")
+	.attr("width",pie_width)
+	.attr("height",pie_height)
+	.selectAll("g")
+	.attr("transform",`translate(${pie_width/2},${pie_height/2})`)
+
+	let piePath=d3.arc()
+	.outerRadius(pie_height/2-padding)
+	.innerRadius(0)
+
+	let pieChart=d3.select(".pieChart")
+	.selectAll(".arc")
+	.attr("d",piePath)
+}
+
+function setColors(){
+	let colors = ["#f1c40f", "#e67e22", "#e74c3c", "#c0392b"];
+
+	let domains = {
 	emissions: [0, 2.5e5, 1e6, 5e6],
 	emissionsPerCapita: [0, 0.5, 2, 10]
 	};
 
-	var scale=d3.scaleLinear()
+	let scale=d3.scaleLinear()
                         .domain(domains[valueToCheck])
                         .range(colors);
 
@@ -99,14 +194,11 @@ function setColors(){
 	.remove()
 
 	d3.select("#map")
-	.attr("width",width)
-	.attr("height",height)
 	.selectAll(".country")
 	.data(geoData)
 	.enter()
 	.append("path")
 	.classed("country",true)
-	.attr("d",path)
 	.on("click",d=>{
 		countryId=d.properties.countryCode
 		drawCountryChart()
@@ -128,6 +220,8 @@ function setColors(){
 		.style("opacity",0)
 	})
 
+	resizeMap();
+
 	d3.selectAll(".country")
 	.transition()
 	.duration(1)
@@ -138,24 +232,18 @@ function setColors(){
 }
 
 function drawPieChart(){
-	let padding=25;
-	pie_width=d3.select("#pieChartContainer").node().getBoundingClientRect().width*0.8;
-	pie_height=pie_width;
 
-	var continents=["Asia","Europe","Africa","Americas","Australia","Oceania","NN"]
+	let continents=["Africa","Americas","Asia","Europe","Australia","Oceania","NN"]
 
-	var pieColorScale=d3.scaleOrdinal()
+	let pieColorScale=d3.scaleOrdinal()
 	.domain(continents)
-	.range(["green","blue","yellow","red","orange","purple","grey"])
+	.range(["#ffe600","#f1c40f", "#e67e22", "#e74c3c", "#c0392b","#8b2217","grey"])
 
-	var chart=d3.select("#pieChart")
-	.attr("width",pie_width)
-	.attr("height",pie_height)
+	let chart=d3.select("#pieChart")
 	.append("g")
-	.attr("transform",`translate(${pie_width/2},${pie_height/2})`)
 	.classed("pieChart",true)
 
-	var arcs=d3.pie()
+	let arcs=d3.pie()
 	.value(d=>d.properties[valueToCheck])
 	.sort((a,b)=>{
 		if (a.properties.continent>b.properties.continent) return 1
@@ -164,11 +252,7 @@ function drawPieChart(){
 	})
 	(geoData)
 
-	var piePath=d3.arc()
-	.outerRadius(pie_height/2-padding)
-	.innerRadius(0)
-
-	var pieChart=d3.select(".pieChart")
+	let pieChart=d3.select(".pieChart")
 	.selectAll(".arc")
 	.data(arcs)
 
@@ -185,46 +269,68 @@ function drawPieChart(){
 	.attr("fill",d=>{
 		return pieColorScale(d.data.properties.continent)
 	})
-	.attr("d",piePath)
+	.on("click",d=>{
+		countryId=d.data.properties.countryCode
+		drawCountryChart()
+	})
+	.on("mouseover",d=>{
+		d3.select(".tooltip")
+		.html(`
+			<p>Continent: ${d.data.properties.continent}</p>
+			<p>Country: ${d.data.properties.country}</p>
+			<p>Emissions: ${d.data.properties.emissions}</p>
+			<p>Emissions Per Capita: ${d.data.properties.emissionsPerCapita}</p>
+			`)
+		.style("opacity",1)
+		.style("top",d3.event.y+"px")
+		.style("left",d3.event.x+"px")
+	})
+	.on("mouseout",()=>{
+		d3.select(".tooltip")
+		.style("opacity",0)
+	})
+
+	resizePieChart()
 }
 
 function drawCountryChart(){
-	var countryData=emmissionsData.filter(d=>d.countryCode===countryId)
-	country_width=d3.select("#countryChartContainer").node().getBoundingClientRect().width*0.8;
-	country_height=country_width
-	var barPadding=5;
-	let padding=50;
-
+	let countryData=emmissionsData.filter(d=>d.countryCode===countryId)
 	minYear=d3.min(countryData, d=>d.year)
 	maxYear=d3.max(countryData, d=>d.year)
 
-	var xScale=d3.scaleLinear()
+	xScale=d3.scaleLinear()
 	.domain(d3.extent(countryData,d=>d.year))
-	.range([padding,country_width-padding])
 
-	var yearArr=[]
-	for(let i=minYear;i<=maxYear;i++){
+	let yearArr=[]
+	for(let i=minYear;i<=maxYear+1;i++){
 		yearArr.push(i)
 	}
 
-	var histogram=d3.histogram()
+	let histogram=d3.histogram()
 	.domain(xScale.domain())
 	.thresholds(yearArr)
 	.value(d=>d["year"])
 
-	var bins=histogram(countryData)
 
-	var yScale=d3.scaleLinear()
+
+	let bins=histogram(countryData)
+
+	yScale=d3.scaleLinear()
 	.domain([0,d3.max(bins,d=>d[0][valueToCheck])])
-	.range([country_height-padding,padding])
 
-	var xAxis=d3.axisBottom(xScale)
-	.tickFormat(d3.format(".0f"))
-	var yAxis=d3.axisLeft(yScale);
+	d3.select("#countryChart")
+	.select(".countryLabel")
+	.remove()
 
-	var bars=d3.select("#countryChart")
-	.attr("width",country_width)
-	.attr("height",country_height)
+	let text=d3.select("#countryChart")
+	.append("text")
+	.classed("countryLabel",true)
+	.text(countryData[0]["country"]+", "+countryData[0]["continent"])
+	.attr("y","20px")
+	.attr("font-size", "20px")
+	.attr("fill", "black");
+
+	let bars=d3.select("#countryChart")
 	.selectAll(".bar")
 	.data(bins)
 
@@ -232,7 +338,7 @@ function drawCountryChart(){
 	.exit()
 	.remove()
 
-	var g=bars
+	let g=bars
 	.enter()
 	.append("g")
 	.classed("bar", true)
@@ -243,27 +349,11 @@ function drawCountryChart(){
 	g
 	.merge(bars)
 	.select("rect")
-	.attr("x",(d,i)=>xScale(d.x0))
-	.attr("y",d=>yScale(d[0][valueToCheck]))
-	.attr("width",d=>xScale(d.x1)-xScale(d.x0)-barPadding)
-	.attr("height",d=>{
-		return country_height-padding-yScale(d[0][valueToCheck])
-	})
-	.attr("fill","green")
+	.attr("fill","#c0392b")
 
 	d3.select("#countryChart")
 	.selectAll(".axis")
 	.remove()
 
-	d3.select("#countryChart")
-	.append("g")
-	.classed("axis",true)
-	.attr("transform","translate(0,"+(country_height-padding)+")")
-	.call(xAxis)
-
-	d3.select("#countryChart")
-	.append("g")
-	.classed("axis",true)
-	.attr("transform","translate("+padding+","+(0)+")")
-	.call(yAxis)
+	resizeCountryChart()
 }
